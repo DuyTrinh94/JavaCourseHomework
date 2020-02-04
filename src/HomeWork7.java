1450cd1340c26a81cf8a938b4ab0cec70183efb6
Duy Trinh <duytrinh@Smartdevs-iMac.local>
2020-02-04 12:30:59 +0700

    Add Homework7

--- src/HomeWork7.java
+++ src/HomeWork7.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class HomeWork7 {
 	public static final char SPACE = ' ';
 	private static char chessInBoard[][] = new char[3][3];
+	private static char chessWin = SPACE;
 	
+	//Display blank board
 	public static void blankBoard() {
 		for(int i=0; i<3; i++) {
 			for(int j=0; j<3; j++) {
@@ -12,6 +14,8 @@ public class HomeWork7 {
 		}
 		boardWithChess();
 	}
+	
+	//Display board
 	public static void printLineLetters() {
 		System.out.print("  ");
 		for(int i=1; i<=3; i++) {
@@ -63,60 +67,152 @@ public class HomeWork7 {
 			System.out.println("|");
 		}
 	}
+	
+	//check win
+	public static boolean isWin() {
+		
+		//check row
+		for(int i=0; i<3; i++) {
+			if(chessInBoard[i][0]==chessInBoard[i][1] && chessInBoard[i][0]==chessInBoard[i][2] && chessInBoard[i][0]!=SPACE) {
+				chessWin = chessInBoard[i][0];
+				break;
+			}
+		}
+		
+		//check column
+		for(int i=0; i<3; i++) {
+			if(chessInBoard[0][i]==chessInBoard[1][i] && chessInBoard[0][i]==chessInBoard[2][i] && chessInBoard[0][i]!=SPACE) {
+				chessWin = chessInBoard[0][i];
+				break;
+			}
+		}
+		
+		//check diagonals
+		if(chessInBoard[0][0]==chessInBoard[1][1] && chessInBoard[0][0]==chessInBoard[2][2] && chessInBoard[0][0]!=SPACE) {
+			chessWin = chessInBoard[0][0];
+		}
+		if(chessInBoard[0][2]==chessInBoard[1][1] && chessInBoard[0][2]==chessInBoard[2][0] && chessInBoard[0][2]!=SPACE) {
+			chessWin = chessInBoard[0][2];
+		}
+		
+		if(chessWin=='X' || chessWin=='O') {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	//check board full with chess
+	public static boolean isFull() {
+		int count=0;
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				if(chessInBoard[i][j]!=SPACE) {
+					count++;
+				}
+			}
+		}
+		if(count==9) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		
+		//Display header
 		System.out.println("**********************************");
 		System.out.print  ("*          GAME CARO 3x3         *\n");
 		System.out.println("**********************************\n");
 		
+		//Input player name
 		System.out.print("Nhap ten player 1: ");
 		String player1Name = scan.nextLine();
 		System.out.print("Nhap ten player 2: ");
 		String player2Name = scan.nextLine();
 		
+		//Display player name and chess
 		System.out.println("[1] " + player1Name + ": O");
 		System.out.println("[2] " + player2Name + ": X");
 		
+		//Select player first
 		boolean isPlayer1 = true;
 		int temp;
 		do {
-			System.out.println("Chon nguoi di truoc [Nhan 1 hoac 2]: ");
+			System.out.print("Chon nguoi di truoc [Nhan 1 hoac 2]: ");
 			temp = scan.nextInt();
 			if(temp == 1) {
 				isPlayer1 = true;
-			}else if(temp == 2){
+			}else{
 				isPlayer1 = false;
 			}
 		}while(temp!=1 && temp!=2);
 		
-		
+		//GAME STARTED
 		System.out.println("************GAME STARTED**********");
 		
+		//display blank board
 		blankBoard();
 		System.out.println("\n");
-
+		
+		//Declare coordinate of chess
 		char coorColumn = ' ';
 		char coorRow = ' ';
 		String coordinate;
-		do {
-			if(isPlayer1) {
-				System.out.print("[Luot cua " + player1Name + " ] Chon vi tri: ");
-			}else {
-				System.out.print("[Luot cua " + player2Name + " ] Chon vi tri: ");
-			}
-			coordinate = scan.nextLine();
-			if(coordinate.length() == 2) {
-				coorColumn = coordinate.split("")[0].toCharArray()[0];
-				coorRow = coordinate.split("")[1].toCharArray()[0];
-			}			
-		}while(((int)coorColumn-64 < 1 || (int)coorColumn-64 > 3) ||
-				((int)coorRow-48 < 1 || (int)coorRow-48 > 3) ||
-				coordinate.length()!=2 ||
-				chessInBoard[coorRow-49][coorColumn-65]!=SPACE);
 		
-		chessInBoard[coorRow-49][coorColumn-65] = 'X';
-		boardWithChess();
-
+		while(!isWin() && !isFull()) {
+			boolean displayAgain = false;
+			do {
+				if(!displayAgain) {
+					if(isPlayer1) {
+						System.out.print("[Luot cua " + player1Name + "] Chon vi tri: ");
+					}else {
+						System.out.print("[Luot cua " + player2Name + "] Chon vi tri: ");
+					}
+				}else {
+					System.out.println("Vi tri khong dung, hay nhap lai!");
+					if(isPlayer1) {
+						System.out.print("[Luot cua " + player1Name + "] Chon vi tri: ");
+					}else {
+						System.out.print("[Luot cua " + player2Name + "] Chon vi tri: ");
+					}
+				}
+				coordinate = scan.next();
+				if(coordinate.length() == 2) {
+					coorColumn = coordinate.split("")[0].toCharArray()[0];
+					coorRow = coordinate.split("")[1].toCharArray()[0];
+				}
+				displayAgain = true;
+			}while(((int)coorColumn-64 < 1 || (int)coorColumn-64 > 3) || //validation for selecting location of chess
+					((int)coorRow-48 < 1 || (int)coorRow-48 > 3) ||
+					coordinate.length()!=2 ||
+					chessInBoard[coorRow-49][coorColumn-65]!=SPACE);
+			
+			//Add chess into matrix
+			if(isPlayer1) {
+				chessInBoard[coorRow-49][coorColumn-65] = 'O';
+				isPlayer1 = false;
+			}else {
+				chessInBoard[coorRow-49][coorColumn-65] = 'X';
+				isPlayer1 = true;
+			}
+			
+			//Display board with chess
+			boardWithChess();
+			System.out.print("\n");
+		}
+		
+		if(isWin() && chessWin=='O') {
+			System.out.println("********** " + player1Name + " win **********");
+		}else if(isWin() && chessWin=='X') {
+			System.out.println("********** " + player2Name + " win **********");
+		}
+		
+		if(!isWin() && isFull()) {
+			System.out.println("********** DRAWN **********" );
+		}
+		scan.close();
 	}
 
 }
